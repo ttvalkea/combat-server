069e93e1ed51c2cb6d331a093b43330b33bce43d
Tuomas Valkeapää <tuomas.valkeapaa@codento.com>
2020-03-09 14:44:55 +0200

    Add new unit tests

--- CombatServer.Tests/UnitTests.cs
+++ CombatServer.Tests/UnitTests.cs
@@ -1,30 +1,47 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
-using Moq;
-using System;
-using System.Threading.Tasks;
 using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CombatServer.Tests
 {
     public class UnitTests
     {
-        private CombatHub hub;
-        public UnitTests()
-        {
-            //Create a hub with mock clients
-            Mock<IHubCallerClients> mockClients = new Mock<IHubCallerClients>();
-            Mock<IClientProxy> mockClientProxy = new Mock<IClientProxy>();
-            mockClients.Setup(clients => clients.All).Returns(mockClientProxy.Object);
-            hub = new CombatHub() { Clients = mockClients.Object };
-        }
-
         [Fact]
         public void GetNewTagItemShouldReturnANewTagItem()
         {
             var result = GameMechanics.GetNewTagItem();
             var expected = typeof(NewTagItem);
-            Assert.Equal(expected, result.GetType());
+            expected.Should().Be(result.GetType());
         }
+
+        [Fact]
+        public void GetIdShouldReturnUniqueIds()
+        {
+            var id1 = Utils.GetId();
+            var id2 = Utils.GetId();
+            var id3 = Utils.GetId();
+            var id4 = Utils.GetId();
+            var id5 = Utils.GetId();
+            var listOfIds = new List<string>() { id1, id2, id3, id4, id5 };
+
+            foreach (var id in listOfIds)
+            {
+                var otherIds = listOfIds.Where(x => x != id).ToList();
+                otherIds.Count.Should().Be(listOfIds.Count - 1);
+                foreach (var otherId in otherIds)
+                {
+                    id.Should().NotBe(otherId);
+                }
+            }            
+        }
+
+        [Fact]
+        public void GetObstaclesShouldGenerateNewObstacles()
+        {
+            GameMechanics.GetObstacles(true);
+            PersistingValues.Obstacles.Count.Should().BeGreaterOrEqualTo(Constants.OBSTACLE_AMOUNT_MIN);
+        }
+        
     }
 }
